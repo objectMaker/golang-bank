971fa46c0c62490df85a54900601b3789bad099c
objectMaker <528154320@qq.com>
2023-11-28 23:35:19 +0800

    add test getAccount func and split the create account to single func

--- db/sqlc/accout_test.go
+++ db/sqlc/accout_test.go
@@ -2,25 +2,45 @@ package db
 
 import (
 	"context"
+	"log"
 	"testing"
 
 	"github.com/objectMaker/golang-bank/utils"
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateAccount(t *testing.T) {
-	var inputAccount CreateAccountParams
-	inputAccount.Owner = utils.RandomName()
-	inputAccount.Balance = utils.RandomInt(1, 100)
-	inputAccount.Currency = utils.RandomCurrency()
-
+func createTestAccount(t *testing.T) Account {
+	inputAccount := CreateAccountParams{
+		Owner:    utils.RandomName(),
+		Balance:  utils.RandomInt(1, 100),
+		Currency: utils.RandomCurrency(),
+	}
 	newAccount, err := testStore.CreateAccount(context.Background(), inputAccount)
 
 	if err != nil {
-		t.Error(err)
+		log.Fatal(err)
 	}
-
 	require.Equal(t, inputAccount.Owner, newAccount.Owner)
 	require.Equal(t, inputAccount.Balance, newAccount.Balance)
 	require.Equal(t, inputAccount.Currency, newAccount.Currency)
+	return newAccount
+}
+
+func TestCreateAccount(t *testing.T) {
+	createTestAccount(t)
+}
+
+func TestGetAccount(t *testing.T) {
+	//create new account
+	newAccount := createTestAccount(t)
+	//query the new account by account id
+	queryAccount, err := testStore.GetAccount(context.Background(), newAccount.ID)
+	if err != nil {
+		log.Fatal("get account error", err)
+	}
+	require.Equal(t, newAccount.ID, queryAccount.ID)
+	require.Equal(t, newAccount.Owner, queryAccount.Owner)
+	require.Equal(t, newAccount.Balance, queryAccount.Balance)
+	require.Equal(t, newAccount.CreatedAt, queryAccount.CreatedAt)
+	require.Equal(t, newAccount.Currency, queryAccount.Currency)
 }
