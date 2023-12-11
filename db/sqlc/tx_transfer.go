e7a80c2446ef43b5c9de9769392e8e18155757d8
objectMaker <528154320@qq.com>
2023-12-11 22:06:05 +0800

    add the txTransfer func

--- db/sqlc/tx_transfer.go
+++ db/sqlc/tx_transfer.go
@@ -1,5 +1,7 @@
 package db
 
+import "context"
+
 type TransferReq struct {
 	FromAccountID int64 `json:"from_account_id"`
 	ToAccountID   int64 `json:"to_account_id"`
@@ -13,4 +15,53 @@ type TransferRes struct {
 	FromAccount Account  `json:"from_account"`
 	ToEntry     Entry    `json:"to_entry"`
 	FromEntry   Entry    `json:"from_entry"`
+}
+
+// create a transfer
+func (store *SqlStore) txTransfer(ctx context.Context, req *TransferReq) {
+	store.execTx(ctx, func(q *Queries) error {
+		var transferRes TransferRes
+		var err error
+		//first: create a transfer
+		transferRes.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
+			FromAccountID: req.FromAccountID,
+			ToAccountID:   req.ToAccountID,
+			Amount:        req.Account,
+		})
+		if err != nil {
+			return err
+		}
+		//fourth: create to entry
+		transferRes.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: req.ToAccountID,
+			Amount:    req.Account,
+		})
+		if err != nil {
+			return err
+		}
+		//fifth: create from entry
+		transferRes.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: req.FromAccountID,
+			Amount:    -req.Account,
+		})
+		if err != nil {
+			return err
+		}
+		//TODO: change user balance
+		// //second: get to account to update its balance
+		// toAccount, err := q.GetAccount(ctx, req.ToAccountID)
+		// if err != nil {
+		// 	return err
+		// }
+		// transferRes.ToAccount = toAccount
+		// //third: get from account to update its balance
+		// fromAccount, err := q.GetAccount(ctx, req.FromAccountID)
+		// if err != nil {
+		// 	return err
+		// }
+		// transferRes.FromAccount = fromAccount
+
+		//if everything fine return nil
+		return nil
+	})
 }
